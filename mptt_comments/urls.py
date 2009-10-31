700a0b477c7b2cac10486c2a3d961aee697bcc3d
Florent Messa <florent.messa@gmail.com>
2009-10-31 12:54:44 +0100

    rename url name in url.py to standardize

--- mptt_comments/urls.py
+++ mptt_comments/urls.py
@@ -5,31 +5,31 @@ from django.conf import settings
 urlpatterns = patterns('mptt_comments.views',
     url(r'^new/(\d+)/$',
         'new_comment',
-        name='new-comment'
+        name='new_comment'
     ),
     url(r'^reply/(\d+)/$',
         'new_comment',
-        name='comment-reply'
+        name='comment_reply'
     ),
     url(r'^post/$',
         'post_comment',
-        name='comments-post-comment'
+        name='comments_post_comment'
     ),
     url(r'^posted-ajax/$',
         'comment_done_ajax',
-        name='comments-comment-done-ajax'
+        name='comments_comment_done_ajax'
     ),
     url(r'^more/(\d+)/$',
         'comments_more',
-        name='comments-more'
+        name='comments_more'
     ),
     url(r'^replies/(\d+)/$',
         'comments_subtree',
-        name='comments-subtree'
+        name='comments_subtree'
     ),
     url(r'^detail/(\d+)/$',
         'comments_subtree',
-        name='comment-detail',
+        name='comment_detail',
         kwargs={'include_self': True, 'include_ancestors': True}
     )
     
